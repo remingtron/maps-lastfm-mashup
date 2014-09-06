a4ba93c457cfdb3de01d467d58a57c7808d36c43
Remington Davenport <remjob14@gmail.com>
2014-09-06 16:26:07 -0400

    adding missing spring dependencies... not sure why these are missing from cache plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -51,6 +51,8 @@ grails.project.dependency.resolution = {
         // runtime 'mysql:mysql-connector-java:5.1.29'
         runtime 'org.postgresql:postgresql:9.3-1101-jdbc41'
         test "org.grails:grails-datastore-test-support:1.0-grails-2.4"
+        compile 'org.springframework:spring-aop:4.0.6.RELEASE'
+        compile 'org.springframework:spring-expression:4.0.6.RELEASE'
     }
 
     plugins {
