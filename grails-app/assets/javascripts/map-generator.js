178cad32fab71c1061a7c082311dca69fa7b7b35
Remington Davenport <remington.davenport@gmail.com>
2014-09-22 12:46:40 -0400

    first step at geolocation

--- grails-app/assets/javascripts/map-generator.js
+++ grails-app/assets/javascripts/map-generator.js
@@ -1,6 +1,6 @@
 var MapGenerator = (function() {
 
-    var mapsWrapper, $;
+    var map, mapsWrapper, $;
 
     var init = function(mapsWrapperIn, jquery) {
         $ = jquery;
@@ -8,11 +8,24 @@ var MapGenerator = (function() {
     };
 
     var initializeMap = function() {
+        var defaultCenter = mapsWrapper.createLatLng(-34.397, 150.644);
         var mapOptions = {
-          center: mapsWrapper.createLatLng(-34.397, 150.644),
+          center: defaultCenter,
           zoom: 8
         };
-        var map = mapsWrapper.createMap($("#map-canvas")[0], mapOptions);
+        map = mapsWrapper.createMap($("#map-canvas")[0], mapOptions);
+        centerMap();
+    };
+
+    var centerMap = function() {
+        if (Html5Support.supportsGeolocation()) {
+            Html5Support.getCurrentPosition(function(position) {
+                initialLocation = mapsWrapper.createLatLng(position.coords.latitude, position.coords.longitude);
+                map.setCenter(initialLocation);
+            }, function(error) {
+
+            });
+        }
     };
 
     return {
@@ -20,3 +33,13 @@ var MapGenerator = (function() {
         initializeMap: initializeMap
     };
 })();
+
+var Html5Support = Html5Support || {};
+
+Html5Support.supportsGeolocation = function() {
+    return 'geolocation' in navigator;
+};
+
+Html5Support.getCurrentPosition = function(success, error) {
+    navigator.geolocation.getCurrentPosition(success, error);
+};
--- test/unit/javascript/spec/MapGeneratorSpec.js
+++ test/unit/javascript/spec/MapGeneratorSpec.js
@@ -1,9 +1,12 @@
 describe("MapGenerator", function() {
 
-    var MapsWrapper;
+    var MapsWrapper, sampleMap;
 
     beforeEach(function() {
         loadFixtures("MapGeneratorFixture.html");
+
+        sampleMap = { setCenter: function() {} };
+
         MapsWrapper = {
             createLatLng: function(latitude, longitude) {
                 return {latitude: latitude, longitude: longitude};
@@ -13,12 +16,36 @@ describe("MapGenerator", function() {
             }
         };
         MapGenerator.init(MapsWrapper, $j);
-        spyOn(MapsWrapper, "createMap");
+        spyOn(MapsWrapper, "createMap").and.returnValue(sampleMap);
+        spyOn(sampleMap, "setCenter");
     });
 
-    it("creates a map", function() {
+    it("creates a map centered in Australia if browser does not support geolocation", function() {
+
+        spyOn(Html5Support, "supportsGeolocation").and.returnValue(false);
+
         MapGenerator.initializeMap();
-        expect(MapsWrapper.createMap).toHaveBeenCalledWith($j('#map-canvas')[0], {center: {latitude: -34.397, longitude: 150.644}, zoom: 8});
+
+        expectMapToBeCreated();
     });
+
+    it("creates a map centered on the user's current location if geolocation success", function() {
+
+        spyOn(Html5Support, "supportsGeolocation").and.returnValue(true);
+        spyOn(Html5Support, "getCurrentPosition").and.callFake(function() {
+            var position = { coords: { latitude: 32, longitude: -12 } };
+            arguments[0](position);
+        });
+
+        MapGenerator.initializeMap();
+
+        expectMapToBeCreated();
+        expect(sampleMap.setCenter).toHaveBeenCalledWith({latitude: 32, longitude: -12});
+    });
+
+    var expectMapToBeCreated = function() {
+        expect(MapsWrapper.createMap).toHaveBeenCalledWith($j('#map-canvas')[0], {center: {latitude: -34.397, longitude: 150.644}, zoom: 8});
+    };
+
 
 });
