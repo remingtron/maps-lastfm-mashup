2938cd77d836cad770e47bf97c1cc83ee73cb94b
Remington Davenport <remington.davenport@gmail.com>
2014-11-14 16:17:08 -0500

    starting events retriever

---
+++ grails-app/assets/javascripts/events-retriever.js
@@ -0,0 +1,17 @@
+var EventsRetriever = (function() {
+
+    var $;
+
+    var init = function(jquery) {
+        $ = jquery;
+    };
+
+    var retrieveEvents = function() {
+    };
+
+    return {
+        init: init;
+        retrieveEvents: retrieveEvents;
+    };
+
+})();
---
+++ test/unit/javascript/spec/EventsRetrieverSpec.js
@@ -0,0 +1,5 @@
+describe("EventsRetriever", function() {
+
+    
+
+});
